70974e66a1b9297c307f31a6b64c7fb35b363a8c
Shiven Patel <132844761+shivenpatel399@users.noreply.github.com>
2024-04-13 09:17:50 -0700

    Update and rename Advanced_I2C.cp to Advanced_I2C.ino

--- Advanced_I2C.ino
+++ Advanced_I2C.ino
@@ -1,9 +1,8 @@
 /*
 Advanced_I2C.ino
-Brian R Taylor
-brian.taylor@bolderflight.com
+Shiven Patel
 
-Copyright (c) 2017 Bolder Flight Systems
+Copyright (c) 2024 SHIVEN PATEL
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
 and associated documentation files (the "Software"), to deal in the Software without restriction, 
