5096d3954e43366f19f25fef9f69720e2ba139d9
Shiven Patel <132844761+shivenpatel399@users.noreply.github.com>
2024-04-13 09:22:31 -0700

    Licencing and Update Sketch

--- TESTENVIRONMENT.ino
+++ TESTENVIRONMENT.ino
@@ -1,3 +1,27 @@
+/*
+TESTENVIRONMENT.ino
+Shiven Patel
+
+Copyright (c) 2024 SHIVEN PATEL
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+and associated documentation files (the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or 
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+// This Sketch was only for test, no real code. Reference Code Logics here
+
 #include <Wire.h>    //Include wire library 
 #include <MPU6050_light.h>  //Include library for MPU communication
 #include <Adafruit_GFX.h>
