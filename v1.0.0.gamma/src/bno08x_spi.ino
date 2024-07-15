280a58115e4541b33cbca621f59d5f8141521cd8
Shiven Patel <132844761+shivenpatel399@users.noreply.github.com>
2024-07-15 13:42:05 -0700

    Add files via upload

---
+++ v1.0.0.gamma/src/bno08x_spi.ino
@@ -0,0 +1,106 @@
+/*
+  Using the BNO08x IMU
+
+  This example shows how to communicate with the sensor over SPI.
+
+  Hardware Connections:
+  IoT RedBoard --> BNO08x
+  D5  --> CS
+  PICO --> SI
+  POCI --> SO
+  SCK  --> SCK
+  A4  --> INT
+  A5  --> RST
+  3V3  --> 3V3
+  GND  --> GND
+
+  BNO08x "mode" pins set for SPI:
+  PSO --> 3V3
+  PS1 --> 3V3
+
+  Serial.print it out at 115200 baud to serial monitor.
+
+*/
+
+//#include <Wire.h>
+
+#include "SparkFun_BNO08x_Arduino_Library.h"  // CTRL+Click here to get the library: http://librarymanager/All#SparkFun_BNO08x
+BNO08x myIMU;
+
+// For SPI, we need some extra pins defined:
+// Note, these can be other GPIO if you like.
+#define BNO08X_CS   5
+#define BNO08X_INT  A4
+#define BNO08X_RST  A5
+
+void setup() {
+  Serial.begin(115200);
+  
+  while(!Serial) delay(10); // Wait for Serial to become available.
+  // Necessary for boards with native USB (like the SAMD51 Thing+).
+  // For a final version of a project that does not need serial debug (or a USB cable plugged in),
+  // Comment out this while loop, or it will prevent the remaining code from running.
+  
+  Serial.println();
+  Serial.println("BNO08x Read Example");
+
+  if (myIMU.beginSPI(BNO08X_CS, BNO08X_INT, BNO08X_RST) == false) {
+    Serial.println("BNO08x not detected. Check your jumpers and the hookup guide. Freezing...");
+    while (1)
+      ;
+  }
+  Serial.println("BNO08x found!");
+
+  setReports();
+
+  Serial.println("Reading events");
+  delay(100);
+}
+
+// Here is where you define the sensor outputs you want to receive
+void setReports(void) {
+  Serial.println("Setting desired reports");
+  if (myIMU.enableRotationVector() == true) {
+    Serial.println(F("Rotation vector enabled"));
+    Serial.println(F("Output in form i, j, k, real, accuracy"));
+  } else {
+    Serial.println("Could not enable rotation vector");
+  }
+  delay(100); // This delay allows enough time for the BNO086 to accept the new 
+              // configuration and clear its reset status
+}
+
+void loop() {
+  delay(10);
+
+  if (myIMU.wasReset()) {
+    Serial.print("sensor was reset ");
+    setReports();
+  }
+
+  // Has a new event come in on the Sensor Hub Bus?
+  if (myIMU.getSensorEvent() == true) {
+
+    // is it the correct sensor data we want?
+    if (myIMU.getSensorEventID() == SENSOR_REPORTID_ROTATION_VECTOR) {
+
+      float quatI = myIMU.getQuatI();
+      float quatJ = myIMU.getQuatJ();
+      float quatK = myIMU.getQuatK();
+      float quatReal = myIMU.getQuatReal();
+      float quatRadianAccuracy = myIMU.getQuatRadianAccuracy();
+
+      Serial.print(quatI, 2);
+      Serial.print(F(","));
+      Serial.print(quatJ, 2);
+      Serial.print(F(","));
+      Serial.print(quatK, 2);
+      Serial.print(F(","));
+      Serial.print(quatReal, 2);
+      Serial.print(F(","));
+      Serial.print(quatRadianAccuracy, 2);
+
+      Serial.println();
+    }
+  }
+}
