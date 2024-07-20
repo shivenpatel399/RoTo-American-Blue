010e68b742606a3891270c906c853eedf90b1189
Shiven Patel <132844761+shivenpatel399@users.noreply.github.com>
2024-07-19 17:43:48 -0700

    Update bno08x_spi.ino

--- v1.0.0.gamma/src/bno08x_spi.ino
+++ v1.0.0.gamma/src/bno08x_spi.ino
@@ -1,7 +1,12 @@
 /*
   Using the BNO08x IMU
 
-  This example shows how to communicate with the sensor over SPI.
+  This example shows how check for a "Reset Complete" message from the sensor,
+  which is helpful when used in tandem with requestResetReason() and
+  getResetReason(). The sensor will be reset each time 100 readings are received
+  to demonstrate.
+
+  It outputs the x/y/z parts of the gyro.
 
   Hardware Connections:
   IoT RedBoard --> BNO08x
@@ -18,11 +23,7 @@
   PSO --> 3V3
   PS1 --> 3V3
 
-  Serial.print it out at 115200 baud to serial monitor.
-
 */
-
-//#include <Wire.h>
 
 #include "SparkFun_BNO08x_Arduino_Library.h"  // CTRL+Click here to get the library: http://librarymanager/All#SparkFun_BNO08x
 BNO08x myIMU;
@@ -32,6 +33,11 @@ BNO08x myIMU;
 #define BNO08X_CS   5
 #define BNO08X_INT  A4
 #define BNO08X_RST  A5
+
+int cyclecount = 0;
+
+#define BNO08X_ADDR 0x4B  // SparkFun BNO08x Breakout (Qwiic) defaults to 0x4B
+//#define BNO08X_ADDR 0x4A // Alternate address if ADR jumper is closed
 
 void setup() {
   Serial.begin(115200);
@@ -44,12 +50,18 @@ void setup() {
   Serial.println();
   Serial.println("BNO08x Read Example");
 
+  Wire.begin();
+
   if (myIMU.beginSPI(BNO08X_CS, BNO08X_INT, BNO08X_RST) == false) {
-    Serial.println("BNO08x not detected. Check your jumpers and the hookup guide. Freezing...");
+    Serial.println("BNO08x not detected at default I2C address. Check your jumpers and the hookup guide. Freezing...");
     while (1)
       ;
   }
   Serial.println("BNO08x found!");
+
+  Serial.print(F("Reset Reason: "));
+  printResetReasonName(myIMU.getResetReason());
+  Serial.println();
 
   setReports();
 
@@ -60,47 +72,73 @@ void setup() {
 // Here is where you define the sensor outputs you want to receive
 void setReports(void) {
   Serial.println("Setting desired reports");
-  if (myIMU.enableRotationVector() == true) {
-    Serial.println(F("Rotation vector enabled"));
-    Serial.println(F("Output in form i, j, k, real, accuracy"));
+  if (myIMU.enableGyro() == true) {
+    Serial.println(F("Gyro enabled"));
+    Serial.println(F("Output in form x, y, z, in radians per second"));
   } else {
-    Serial.println("Could not enable rotation vector");
+    Serial.println("Could not enable gyro");
   }
   delay(100); // This delay allows enough time for the BNO086 to accept the new 
               // configuration and clear its reset status
 }
 
 void loop() {
-  delay(10);
+  delay(1);
+  myIMU.serviceBus();
 
+  // One of these will appear at the very start because of the power on reset.
+  // Use requestResetReason() and getResetReason() for the difference between
+  // different resets.
   if (myIMU.wasReset()) {
-    Serial.print("sensor was reset ");
-    setReports();
+    Serial.println(" ------------------ BNO08x has reset. ------------------ ");
+    Serial.print(F("Reset Reason: "));
+    printResetReasonName(myIMU.getResetReason());
+    Serial.println();
+
+    setReports();  // We'll need to re-enable reports after any reset.
   }
 
   // Has a new event come in on the Sensor Hub Bus?
   if (myIMU.getSensorEvent() == true) {
 
     // is it the correct sensor data we want?
-    if (myIMU.getSensorEventID() == SENSOR_REPORTID_ROTATION_VECTOR) {
+    if (myIMU.getSensorEventID() == SENSOR_REPORTID_GYROSCOPE_CALIBRATED) {
 
-      float quatI = myIMU.getQuatI();
-      float quatJ = myIMU.getQuatJ();
-      float quatK = myIMU.getQuatK();
-      float quatReal = myIMU.getQuatReal();
-      float quatRadianAccuracy = myIMU.getQuatRadianAccuracy();
+      cyclecount++;
 
-      Serial.print(quatI, 2);
+      Serial.print(F("["));
+      if (cyclecount < 10) Serial.print(F("0"));
+      Serial.print(cyclecount);
+      Serial.print(F("] "));
+
+      float x = myIMU.getGyroX();
+      float y = myIMU.getGyroY();
+      float z = myIMU.getGyroZ();
+
+      Serial.print(x, 2);
       Serial.print(F(","));
-      Serial.print(quatJ, 2);
+      Serial.print(y, 2);
       Serial.print(F(","));
-      Serial.print(quatK, 2);
-      Serial.print(F(","));
-      Serial.print(quatReal, 2);
-      Serial.print(F(","));
-      Serial.print(quatRadianAccuracy, 2);
+      Serial.print(z, 2);
 
       Serial.println();
+
+      if (cyclecount == 100) {
+        myIMU.softReset();
+        cyclecount = 0;
+      }
     }
   }
+}
+
+
+//Given a number between 0 and 5, print the name of the reset reason
+//1 = POR, 2 = Internal reset, 3 = Watchdog, 4 = External reset, 5 = Other
+void printResetReasonName(byte resetReasonNumber)
+{
+  if(resetReasonNumber == 1) Serial.print("POR");
+  else if(resetReasonNumber == 2) Serial.print("Internal reset");
+  else if(resetReasonNumber == 3) Serial.print("Watchdog");
+  else if(resetReasonNumber == 4) Serial.print("External reset");
+  else if(resetReasonNumber == 5) Serial.print("Other");
 }
