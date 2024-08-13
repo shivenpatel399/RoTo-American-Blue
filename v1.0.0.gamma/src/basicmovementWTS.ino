2be2335c5b3f974ca2e79b5eeb89c8d0a6c8c14d
Shiven Patel <132844761+shivenpatel399@users.noreply.github.com>
2024-08-12 22:39:00 -0700

    Update basicmovementWTS.ino

--- v1.0.0.gamma/src/basicmovementWTS.ino
+++ v1.0.0.gamma/src/basicmovementWTS.ino
@@ -1,6 +1,9 @@
+#include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_BNO08x.h>
+#include <cmath>
 
 
 const int E1 = 3; ///<Motor1 Speed
@@ -23,6 +26,23 @@ volatile int counter_B = 0;
 volatile int counter_C = 0;
 volatile int counter_D = 0;
 
+bool checker = false;
+unsigned long lasttime;
+unsigned long thistime;
+
+#define BNO08X_CS 36
+#define BNO08X_INT 32
+
+
+// #define FAST_MODE
+
+// For SPI mode, we also need a RESET 
+#define BNO08X_RESET 34
+// but not for I2C or UART
+// #define BNO08X_RESET -1
+
+
+
 struct euler_t {
   float yaw;
   float pitch;
@@ -31,6 +51,34 @@ struct euler_t {
 
 Adafruit_BNO08x  bno08x(BNO08X_RESET);
 sh2_SensorValue_t sensorValue;
+
+
+float thecorrection = 0;
+
+float powerA = 0;
+float powerB = 0;
+float powerC = 0;
+float powerD = 0;
+
+float stepcount = 176.00;
+float circumference = 18.221;
+
+float integral = 0;
+float derivative = 0;
+float lastError = 0;
+double kpGain = 0;
+double kiGain = 0;
+double kdGain = 0;
+float Gain = 0;
+float gain = 0;
+float currentAngle = 0;
+float target = 0;
+int targetIterator = 0;
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 #ifdef FAST_MODE
   // Top frequency is reported to be 1000Hz (but freq is somewhat variable)
@@ -47,6 +95,57 @@ void setReports(sh2_SensorId_t reportType, long report_interval) {
     Serial.println("Could not enable stabilized remote vector");
   }
 }
+
+
+
+void setup() {
+  Serial.begin(9600);
+  Wire.begin();
+  Wire.setClock(400000);
+  attachInterrupt(digitalPinToInterrupt(enA), ISR_countA, RISING); 
+  attachInterrupt(digitalPinToInterrupt(enB), ISR_countB, RISING);
+  attachInterrupt(digitalPinToInterrupt(enC), ISR_countC, RISING);
+  attachInterrupt(digitalPinToInterrupt (enD), ISR_countD, RISING);
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for (;;);
+  }
+  pinMode(enA, INPUT_PULLUP);
+  pinMode(enB, INPUT_PULLUP);
+  pinMode(enC, INPUT_PULLUP);
+  pinMode(enD, INPUT_PULLUP);
+  for(int i=3;i<9;i++)
+    pinMode(i,OUTPUT);
+  for(int i=11;i<13;i++)
+    pinMode(i,OUTPUT);
+
+  if (!bno08x.begin_SPI(BNO08X_CS, BNO08X_INT)) {
+    while (1) { delay(10); }
+  }
+
+
+  setReports(reportType, reportIntervalUs);
+
+  delay(100);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println(F("Welcome Shiven!"));
+  display.println(F("Code will run shortly..."));
+  display.display();
+  delay(5000);
+  // PDeast(CMtoSteps(50), 155, 3.2275);
+  northx(CMtoSteps(40),130);
+  eastx(CMtoSteps(40),130);
+  southx(CMtoSteps(40),130);
+  westx(CMtoSteps(40),130);
+} 
+
+
+void loop() {
+  // put code here
+} 
 
 void quaternionToEuler(float qr, float qi, float qj, float qk, euler_t* ypr, bool degrees = false) {
 
@@ -74,104 +173,98 @@ void quaternionToEulerGI(sh2_GyroIntegratedRV_t* rotational_vector, euler_t* ypr
     quaternionToEuler(rotational_vector->real, rotational_vector->i, rotational_vector->j, rotational_vector->k, ypr, degrees);
 }
 
-float target = 0;
-float thecorrection = 0;
-
-float powerA = 0;
-float powerB = 0;
-float powerC = 0;
-float powerD = 0;
-
-float stepcount = 176.00;
-float circumference = 18.221;
-
-float integral = 0;
-float derivative = 0;
-float lastError = 0;
-double kpGain = 0;
-double kiGain = 0;
-double kdGain = 0;
-float Gain = 0;
-float gain = 0;
-
-#define SCREEN_WIDTH 128
-#define SCREEN_HEIGHT 64
-#define OLED_RESET -1
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-
-void setup() {
-  Serial.begin(9600);
-  Wire.begin();
-  Wire.setClock(400000);
-  attachInterrupt(digitalPinToInterrupt(enA), ISR_countA, RISING); 
-  attachInterrupt(digitalPinToInterrupt(enB), ISR_countB, RISING);
-  attachInterrupt(digitalPinToInterrupt(enC), ISR_countC, RISING);
-  attachInterrupt(digitalPinToInterrupt (enD), ISR_countD, RISING);
-  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;);
+void targetAngle() {
+  for (targetIterator = 0; targetIterator < 9; targetIterator++) {
+    if (bno08x.wasReset()) {
+      setReports(reportType, reportIntervalUs);
+    }
+  
+    if (bno08x.getSensorEvent(&sensorValue)) {
+      // in this demo only one report type will be received depending on FAST_MODE define (above)
+      switch (sensorValue.sensorId) {
+        case SH2_ARVR_STABILIZED_RV:
+          quaternionToEulerRV(&sensorValue.un.arvrStabilizedRV, &ypr, true);
+        case SH2_GYRO_INTEGRATED_RV:
+          // faster (more noise?)
+          quaternionToEulerGI(&sensorValue.un.gyroIntegratedRV, &ypr, true);
+          break;
+      }
+    }
+    target = ypr.yaw;
   }
-  pinMode(enA, INPUT_PULLUP);
-  pinMode(enB, INPUT_PULLUP);
-  pinMode(enC, INPUT_PULLUP);
-  pinMode(enD, INPUT_PULLUP);
-  for(int i=3;i<9;i++)
-    pinMode(i,OUTPUT);
-  for(int i=11;i<13;i++)
-    pinMode(i,OUTPUT);
-  display.clearDisplay();
-  display.setTextSize(1);
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
-  display.println(F("Welcome Shiven!"));
-  display.println(F("Code will run shortly..."));
-  display.display();
-  delay(5000);
-  northplus(CMtoSteps(50), 100);
-  eastplus(CMtoSteps(50), 100);
-  southplus(CMtoSteps(50), 100);
-  westplus(CMtoSteps(50), 100);
-  
-  
-
-} 
-
-
-void loop() {
-  // put code here
-} 
+}
 
 void PDeast(int thesteps, int power, float gained) {
   counter_A = 0;
   counter_C = 0;
-  target = ypr.yaw();
+  targetAngle();
+  lasttime = micros();
   while ((thesteps * 2) > counter_A + counter_C) {
-    thecorrection = target - float(ypr.yaw());
-    powerA = power + (gained * thecorrection);
-    powerC = power - (gained * thecorrection);
+    if (bno08x.wasReset()) {
+      setReports(reportType, reportIntervalUs);
+    }
+  
+    if (bno08x.getSensorEvent(&sensorValue)) {
+      // in this demo only one report type will be received depending on FAST_MODE define (above)
+      switch (sensorValue.sensorId) {
+        case SH2_ARVR_STABILIZED_RV:
+          quaternionToEulerRV(&sensorValue.un.arvrStabilizedRV, &ypr, true);
+        case SH2_GYRO_INTEGRATED_RV:
+          // faster (more noise?)
+          quaternionToEulerGI(&sensorValue.un.gyroIntegratedRV, &ypr, true);
+          break;
+      }
+    }
+    currentAngle = float(ypr.yaw);
+    thecorrection = target - currentAngle;
+    powerA = power - (gained * thecorrection);
+    powerC = power + (gained * thecorrection);
     M1_advance(powerA);
     M3_back(powerC);
     display.clearDisplay();
     display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
     display.setCursor(0, 0);
-    display.print(" Counter A: ");
-    display.println(counter_A);
-    display.print(" Counter C: ");
-    display.println(counter_C);
+    display.print(" Expect: ");
+    display.println(target);
+    display.print(" Currnt: ");
+    display.println(currentAngle);
+    display.print(" PowerA: ");
+    display.println(powerA);
+    display.print(" PowerC: ");
+    display.println(powerC);
+    display.print(" Ang Diff: ");
+    display.println(abs(target-currentAngle));
     display.display();
 	  delay(10);
     } 
+  thistime = micros();
   stopWait();
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print(" Expect: ");
+  display.println(target);
+  display.print(" Currnt: ");
+  display.println(currentAngle);
+  display.print(" PowerA: ");
+  display.println(powerA);
+  display.print(" PowerC: ");
+  display.println(powerC);
+  display.print(" Ang Diff: ");
+  display.println(abs(target-currentAngle));
+  display.print(" Time: ");
+  display.println(abs(thistime-lasttime));
+  display.display();
 }
-
+/*
 void PDwest(int thesteps, int power, float gained) {
   counter_A = 0;
   counter_C = 0;
   target = ypr.yaw();
   while ((thesteps * 2) > counter_A + counter_C) {
-    thecorrection = target - float(ypr.yaw());
+    thecorrection = target - float(ypr.yaw);
     powerA = power + (gained * thecorrection);
     powerC = power - (gained * thecorrection);
     M1_back(powerA);
@@ -195,7 +288,7 @@ void PDnorth(int thesteps, int power, float gained) {
   counter_D = 0;
   target = ypr.yaw();
   while ((thesteps * 2) > counter_B + counter_D) {
-    thecorrection = target - float(ypr.yaw());
+    thecorrection = target - float(ypr.yaw);
     powerB = power + (gained * thecorrection);
     powerD = power - (gained * thecorrection);
     M2_back(powerB);
@@ -496,6 +589,98 @@ void southx(float steps, int speed) {
 
 }
 
+
+void eastx(float steps, int speed) {
+  counter_A = 0;
+  counter_B = 0;
+  counter_C = 0;
+  counter_D = 0;
+  while ((steps*4) > counter_A + counter_B + counter_C + counter_D) {
+    	// Set motors to maximum speed
+	  // For PWM maximum possible values are 0 to 255
+	  M1_advance(speed);
+    M2_advance(speed);
+    M3_back(speed);
+    M4_back(speed);
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    display.print(" Counter A: ");
+    display.println(counter_A);
+    display.print(" Counter B: ");
+    display.println(counter_B);
+    display.print(" Counter C: ");
+    display.println(counter_C);
+    display.print(" Counter D: ");
+    display.println(counter_D);
+    display.display();
+	  delay(10);
+    }
+  stopWait();
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print(" Counter A: ");
+  display.println(counter_A);
+  display.print(" Counter B: ");
+  display.println(counter_B);
+  display.print(" Counter C: ");
+  display.println(counter_C);
+  display.print(" Counter D: ");
+  display.println(counter_D);
+  display.print(" Speed: ");
+  display.println(speed);
+  display.display();
+
+}
+
+void westx(float steps, int speed) {
+  counter_A = 0;
+  counter_B = 0;
+  counter_C = 0;
+  counter_D = 0;
+  while ((steps*4) > counter_A + counter_B + counter_C + counter_D) {
+    	// Set motors to maximum speed
+	  // For PWM maximum possible values are 0 to 255
+	  M1_back(speed);
+    M2_back(speed);
+    M3_advance(speed);
+    M4_advance(speed);
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    display.print(" Counter A: ");
+    display.println(counter_A);
+    display.print(" Counter B: ");
+    display.println(counter_B);
+    display.print(" Counter C: ");
+    display.println(counter_C);
+    display.print(" Counter D: ");
+    display.println(counter_D);
+    display.display();
+	  delay(10);
+    }
+  stopWait();
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.print(" Counter A: ");
+  display.println(counter_A);
+  display.print(" Counter B: ");
+  display.println(counter_B);
+  display.print(" Counter C: ");
+  display.println(counter_C);
+  display.print(" Counter D: ");
+  display.println(counter_D);
+  display.print(" Speed: ");
+  display.println(speed);
+  display.display();
+
+}
 
 void M1_advance(char Speed) { ///<Motor1 Advance 
   digitalWrite(M1,LOW);
